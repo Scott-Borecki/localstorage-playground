0dd7191d58ca3b3d6a8661e88a340913f9744535
Scott Borecki <scottborecki@gmail.com>
2021-11-07 13:17:52 -0700

    Implement Add Contact button functionality

--- scripts.js
+++ scripts.js
@@ -1,17 +1,25 @@
 /* Use this area for pseudo - coding:
 
-
-
 */
 
 // Query selectors
+var contactNameInput = document.getElementById('name');
+var contactEmailInput = document.getElementById('email');
 
+var addContactButton = document.getElementById('register-btn');
 
 
 // Event listeners
-
-
-
+addContactButton.addEventListener('click', addContact);
 
 // Functions
+function addContact() {
+  event.preventDefault();
 
+  var newName = contactNameInput.value;
+  var newEmail = contactEmailInput.value;
+  var contactDetails = { "name": newName, "email": newEmail };
+  var contactDetailsString = JSON.stringify(contactDetails);
+
+  localStorage.setItem('contactDetails', contactDetailsString);
+}
