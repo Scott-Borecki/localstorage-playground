286e8fdb53542fedb7cc51b0c5bac7097dd39541
Scott Borecki <scottborecki@gmail.com>
2021-11-07 13:18:10 -0700

    Implement Display Contact button functionality

--- scripts.js
+++ scripts.js
@@ -7,10 +7,13 @@ var contactNameInput = document.getElementById('name');
 var contactEmailInput = document.getElementById('email');
 
 var addContactButton = document.getElementById('register-btn');
+var displayContactButton = document.getElementById('display-btn');
 
+var displayArea = document.querySelector('.display-area');
 
 // Event listeners
 addContactButton.addEventListener('click', addContact);
+displayContactButton.addEventListener('click', displayContact);
 
 // Functions
 function addContact() {
@@ -22,4 +25,18 @@ function addContact() {
   var contactDetailsString = JSON.stringify(contactDetails);
 
   localStorage.setItem('contactDetails', contactDetailsString);
+}
+
+function displayContact() {
+  event.preventDefault();
+
+  var contactDetails = localStorage.getItem('contactDetails');
+  var contactDetailsParsed = JSON.parse(contactDetails);
+
+  displayArea.innerHTML = `
+    <section class="contact-details">
+      <p id="contact-name">Name: ${contactDetailsParsed.name}</p>
+      <p id="contact-email">Email: ${contactDetailsParsed.email}</p>
+    </section>
+  `
 }
